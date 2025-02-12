1c78972029f215907bf6ad66f003963673d6c365
jhhocs <joshuaho1@cpp.edu>
2025-02-11 17:31:18 -0800

    transform features & classes to numbers

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Joshua Ho
+# FILENAME: decision_tree.py
+# SPECIFICATION: Plot a decision tree
 # FOR: CS 4210- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: how long it took you to complete the assignment Start: 9:50 PM Programming
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -17,6 +17,8 @@ db = []
 X = []
 Y = []
 
+attributes = []
+
 #reading the data in a csv file
 with open('contact_lens.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
@@ -24,19 +26,46 @@ with open('contact_lens.csv', 'r') as csvfile:
       if i > 0: #skipping the header
          db.append (row)
          print(row)
+      else:
+         attributes = row
 
 #transform the original categorical training features into numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
 #--> add your Python code here
-# X =
+num_features = len(row) - 1
+d = [dict() for x in range(num_features)]
+for i, row in enumerate(db):
+   X.append([])
+   for x in range(num_features):
+      if row[x] in d[x]:
+         X[i].append(d[x].get(row[x]))
+      else:
+         X[i].append(len(d[x]))
+         d[x][row[x]] = len(d[x])
+# print(d)
+# print(X)
+# for(row in )
 
 #transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2
 #--> addd your Python code here
-# Y =
+d2 = {}
+for row in db:
+  if row[-1] in d2:
+    Y.append(d2.get(row[-1]))
+  else:
+    Y.append(len(d2))
+    d2[row[-1]] = len(d2)
 
-#fitting the decision tree to the data
+# print(d2)
+# print(Y)
+
+# fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
 clf = clf.fit(X, Y)
 
-#plotting the decision tree
-tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
+# plotting the decision tree
+# tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
+
+# Allows code to work for different datasets
+tree.plot_tree(clf, feature_names=attributes, class_names=list(d2.keys()), filled=True, rounded=True)
+
 plt.show()
